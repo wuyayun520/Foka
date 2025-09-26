c4df33be96d779d9967dfffd4ab2b6c9fee4c33b
Charlotte <18631656747@163.com>
2025-09-26 16:31:20 +0800

    Remove workflow files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,9 +9,7 @@ import AppTrackingTransparency
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      
-      let adcode = ["1","3","4"]
-      print("%@",adcode[33])
+     
       DispatchQueue.main.asyncAfter(deadline: .now() + 5.52) {
           if #available(iOS 14, *) {
               ATTrackingManager.requestTrackingAuthorization { status in
