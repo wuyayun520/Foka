c27de46a52095c4954efc70e7228e0752f7fef11
Charlotte <18631656747@163.com>
2025-10-11 14:22:16 +0800

    Remove workflow files

--- ios/Runner/main.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import UIKit
-import ChartFindSilentOpen
-
-UIApplicationMain(CommandLine.argc,
-                  CommandLine.unsafeArgv,
-                  NSStringFromClass(SensitiveFamousApplication.self),
-                  NSStringFromClass(AppDelegate.self))
